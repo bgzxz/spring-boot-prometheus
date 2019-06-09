074d175003ec9ac016906d424e69802d2d4db783
bgzxz <bgzxz1987@gmail.com>
2019-06-09 23:09:47 +0800

    init

---
+++ src/main/java/dev/docbot/OrderController.java
@@ -0,0 +1,29 @@
+package dev.docbot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class OrderController {
+    @GetMapping("/api/queryOrder")
+    public List<String> queryOrder() throws InterruptedException {
+        Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
+        return Collections.EMPTY_LIST;
+    }
+    @GetMapping("/api/createOrder")
+    public void createOrder() throws InterruptedException {
+        Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
+    }
+    @GetMapping("/api/orderDetail")
+    public void orderDetail() throws InterruptedException {
+
+    }
+    @GetMapping("/api/exportOrders")
+    public void exportOrders() throws InterruptedException {
+        Thread.sleep(ThreadLocalRandom.current().nextInt(3000));
+    }
+}
